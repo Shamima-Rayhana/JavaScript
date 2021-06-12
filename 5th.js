61861e41886a377ba91746d8b31274060935305c
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-12 23:17:52 +0600

    String declaration and initialization

---
+++ 5th.js
@@ -0,0 +1,6 @@
+//Declare string variable and initialization
+var a="Shamima";
+var b="Rayhana";
+var x="I said,\"Exam will not held\".";
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+var ourStr="Semister Final\n\t\\Mid\nQuize";
