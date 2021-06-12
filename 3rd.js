00f82fc1981e111886832c85f3d9957227996ae6
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-12 22:51:09 +0600

    Decimal variable and operation between them

---
+++ 3rd.js
@@ -0,0 +1,8 @@
+//Decimal variable
+var decimalNum1,decimalNum2;
+decimalNum1=10;
+
+decimalNum2=20;
+var mul=decimalNum1*decimalNum2;
+var div=decimalNum2/decimalNum1;
+var reminder=15%decimalNum1;
