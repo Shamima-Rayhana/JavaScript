3bf08ca6182757514a1b6eb568c13a655aadceea
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-12 22:52:43 +0600

    Use of short hand operator

---
+++ 4th.js
@@ -0,0 +1,14 @@
+//short hand operator "+=" and "-="
+var a=50,b=30;
+a+=50; //a=a+50;
+a-=10; //a=a-10;
+b+=30; //b=b+30;
+b-=10; //b=b-10;
+
+//short hand operator "*=" and "/="
+var x=10,y=10,z=5;
+x*=10;
+x/=z;
+
+y*=2;
+y/=10;
