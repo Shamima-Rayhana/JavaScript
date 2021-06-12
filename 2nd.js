816d8c6f0ac64e6a56c25f4b44a82b7c21d5bc1c
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-12 09:08:14 +0600

    Arithmetic operation, Increament and Decreament

---
+++ 2nd.js
@@ -0,0 +1,20 @@
+//This is my first JavaScript code
+/*to be patient and learn */
+
+//declaration and initialization
+var a;
+a=4;
+var b=2;
+var c;
+c=b;
+var s="I am Shamima";
+//arithmetic operation
+var sum=a+b;
+var sub=a-b;
+var mul=a*b;
+var div=a/b;
+//increament and decreament
+a++;
+c=c+1;
+b--;
+s=s+"Rayhana";
