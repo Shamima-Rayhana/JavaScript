6560ed9a4faf158776b439dc92904c9910c9868a
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-15 16:27:30 +0600

    Declare global variable

--- 7th.js
+++ 7th.js
@@ -14,3 +14,22 @@ function functionWithArgs(a,b)
   document.write("sum= ",sum);
 }
 functionWithArgs(10,20);
+// Declare Global variable
+var myGlobal=10;
+
+
+function fun1() {
+    var oopsGlobal=5;
+    
+}
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  document.write(output);
+}
