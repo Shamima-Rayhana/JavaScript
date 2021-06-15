35e04dbbc9c6571474a9c91243d7079b92d74fa2
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-15 13:56:17 +0600

    Array declaration and nested array

---
+++ 6th.js
@@ -0,0 +1,6 @@
+//array declaration
+var array=["The num of peanut butter is ", 5];
+var tea=["water", "sugure", "tea leaf","salt"];
+document.write("The result is a cup of tea");
+//nested array
+var food=[["mango", 4], ["banana", 2]];
