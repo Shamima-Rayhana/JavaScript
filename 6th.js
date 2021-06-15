704d16b9cd8c4160b8f08d70c508a008d433c775
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-15 14:14:52 +0600

    access and modify array element

--- 6th.js
+++ 6th.js
@@ -4,3 +4,8 @@ var tea=["water", "sugure", "tea leaf","salt"];
 document.write("The result is a cup of tea");
 //nested array
 var food=[["mango", 4], ["banana", 2]];
+//array element accessing by indexing 
+var favourite=food[2][0];
+//modify array element
+food[1][1]=5;
+var totalBanana=food[1][1];
