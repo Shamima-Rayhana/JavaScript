9d6c0d0709dca9b98f18d8ea85223c8ee2fa3f6b
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-15 16:03:06 +0600

    Passing Values to Functions with Arguments

--- 7th.js
+++ 7th.js
@@ -1,6 +1,16 @@
 //function for reuse 
 function reusableFunction()
 {
-    console.log("Hellow World");
+    //console.log("Hellow World");
+    document.write("Hellow World");
+    document.write("<br>");
 }
 reusableFunction();
+//Passing Values to Functions with Arguments
+function functionWithArgs(a,b)
+{
+  var sum=a+b;
+  //console.log(sum);
+  document.write("sum= ",sum);
+}
+functionWithArgs(10,20);
