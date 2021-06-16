edb242e528b20cab6018e8cc5a470da5916e468a
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-16 19:40:23 +0600

    switch statement with default value

--- 9th.js
+++ 9th.js
@@ -6,18 +6,22 @@ function caseInSwitch(val) {
     case 1:
     answer="alpha";
     document.write(answer);
+    document.write("<br>");
     break;
     case 2:
     answer="beta";
     document.write(answer);
+    document.write("<br>");
     break;
     case 3:
     answer="gamma";
     document.write(answer);
+    document.write("<br>");
     break;
     case 4:
     answer="delta";
     document.write(answer);
+    document.write("<br>");
     break;
   }
   
@@ -25,3 +29,34 @@ function caseInSwitch(val) {
   }
   
   caseInSwitch(1);
+  //switch statement with default value
+  function switchOfStuff(val) {
+    var answer = "";
+   switch(val)
+   {
+     case "a":
+     answer="apple";
+     document.write(answer);
+    document.write("<br>");
+     break;
+     case "b":
+     answer="bird";
+     document.write(answer);
+    document.write("<br>");
+     break;
+     case "c":
+     answer="cat";
+     document.write(answer);
+    document.write("<br>");
+     break;
+     default:
+     answer="stuff";
+     document.write(answer);
+    document.write("<br>");
+     break;
+   }
+  
+    return answer;
+  }
+  
+  switchOfStuff(1);
