7b7354bedca6cb7a93743d40e793a61aaecccfad
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-16 19:34:19 +0600

    switch statement

---
+++ 9th.js
@@ -0,0 +1,27 @@
+//switch statement
+function caseInSwitch(val) {
+    var answer = "";
+  switch(val)
+  {
+    case 1:
+    answer="alpha";
+    document.write(answer);
+    break;
+    case 2:
+    answer="beta";
+    document.write(answer);
+    break;
+    case 3:
+    answer="gamma";
+    document.write(answer);
+    break;
+    case 4:
+    answer="delta";
+    document.write(answer);
+    break;
+  }
+  
+    return answer;
+  }
+  
+  caseInSwitch(1);
