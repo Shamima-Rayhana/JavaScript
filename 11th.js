9de79b38f646d13630c8deaa36bf13b854092443
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-17 17:52:46 +0600

    Add New Properties to a JavaScript Object

--- 11th.js
+++ 11th.js
@@ -45,3 +45,13 @@ var myDog = {
   };
   
   myDog.name="Happy Coder";
+  //Add New Properties to a JavaScript Object
+  var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog["bark"]="woof";
+  
+  
