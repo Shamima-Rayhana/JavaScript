52fbd72f5e7d5b2ccbbafe87a0bb7b98ce1f05d7
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-17 17:33:09 +0600

    Accessing Object Properties with Dot Notation

--- 11th.js
+++ 11th.js
@@ -5,3 +5,13 @@ var myDog = {
     tails: 1,
     friends: ["jcnhd", "kshus", "skfhjgf"]
     };
+    //Accessing Object Properties with Dot Notation
+    // Setup
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  var hatValue = testObj.hat;     
+  var shirtValue = testObj.shirt;  
