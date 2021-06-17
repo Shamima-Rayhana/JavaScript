4ab396cabe0c620aac7bd89f7487e22d700a5e67
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-17 18:56:07 +0600

    Manipulating Complex Objects

--- 11th.js
+++ 11th.js
@@ -89,5 +89,30 @@ function phoneticLookup(val) {
     else
     return "Not Found";
   }
+  //Manipulating Complex Objects
+  var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+    }
+  
+  ];
   
   
