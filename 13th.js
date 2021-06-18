37ef30cde2207e02d3f85a30d887dd68375f4d12
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-18 13:45:56 +0600

    Use Multiple Conditional (Ternary) Operators

--- 13th.js
+++ 13th.js
@@ -92,3 +92,11 @@ function randomFraction() {
   }
   
   checkEqual(1, 2);
+//Use Multiple Conditional (Ternary) Operators
+
+  function checkSign(num) {
+
+    return num>0? "positive": num<0? "negative":"zero";
+    }
+    
+    checkSign(10);
