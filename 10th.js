ca65dd84b3e87de2047af654e3c321a4bb0b3a42
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-18 08:37:56 +0600

    unexpected change

--- 10th.js
+++ 10th.js
@@ -6,11 +6,9 @@ function isLess(a, b) {
   isLess(10, 15);
 
   //Return Early Pattern for Functions
-  // Setup
 function abTest(a, b) {
   if(a<0 || b<0)
   return undefined;
-  
     return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
   }
   
