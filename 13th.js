da2fd296e3ab4856ab3f90c245a3f6484d4d510b
Shamima Rayhana <rumi.cse.hstu@gmail.com>
2021-06-18 13:40:32 +0600

    Use the Conditional (Ternary) Operator

--- 13th.js
+++ 13th.js
@@ -84,3 +84,11 @@ function randomFraction() {
   }
   
   convertToInteger("10011");
+
+//Use the Conditional (Ternary) Operator
+  function checkEqual(a, b) {
+    return a==b? "Equal":"Not Equal";
+  
+  }
+  
+  checkEqual(1, 2);
